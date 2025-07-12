4797d07412d2c6e385d6dacf90d586c2f50248b8
IMRAN HAMZA <imranhamza454@gmail.com>
2025-07-12 10:08:12 +0100

    finished the installment part

--- src/Components/Installment/InstallMate.jsx
+++ src/Components/Installment/InstallMate.jsx
@@ -1,9 +1,15 @@
-
+import  { useState } from "react";
+import CalculatorForm from "./CalculatorForm";
+import PaymentTracker from "./PaymentTracker";
 
 export default function InstallMate() {
+  const [clientData, setClientData] = useState(null);
+
   return (
-    <div>
-      yes this is InstallMate
+    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl font-bold mb-6">InstallMate</h1>
+      <CalculatorForm onCalculate={setClientData} />
+      {clientData && <PaymentTracker client={clientData} />}
     </div>
-  )
+  );
 }
